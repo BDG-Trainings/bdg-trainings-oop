27f912a929d20cec2332b6d2a723a5b24cffb2c7
Sergey Papoyan <spapoyan87@gmail.com>
2019-01-27 14:10:35 +0400

    new_bdg_class

--- src/com/bdg/spapoyan/Data.java
+++ src/com/bdg/spapoyan/Data.java
@@ -1,7 +1,9 @@
 package com.bdg.spapoyan;
-
 public class Data {
-    public static void main (String []args) {
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        int array [] = {15,20,25,28};
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array [i] + ",");
+        }
     }
 }
