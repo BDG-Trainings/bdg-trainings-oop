0bd4430aa730ed90b4b0738fa2d04d8b241e523c
Varduhi <varduhi.qertikyan@gmail.com>
2019-01-28 21:23:38 +0400

    resource management

---
+++ src/com/bdg/vqertikyan/resourcemanagement/MainResourceManagement.java
@@ -0,0 +1,26 @@
+package com.bdg.vqertikyan.resourcemanagement;
+
+public class MainResourceManagement {
+
+    public static void main(String[] args) {
+        Address[] addresses = new Address[1];
+        addresses[0] = new Address("street1", "city1", "state1", "zipCode1");
+
+        Person person = new Person("name", "firstName", "middleName", (short) 25, addresses);
+
+        Employee newEmp = new Employee(1000, person);
+
+        ResourceManagement resourceManagement = new ResourceManagement();
+
+        Department[] departments = new Department[5];
+        departments[0] = new Department("dep1", null, null, 4000);
+        departments[1] = new Department("dep2", null, null, 3000);
+        departments[2] = new Department("dep3", null, null, 5000);
+        departments[3] = new Department("dep4", null, null, 1000);
+        departments[4] = new Department("dep5", null, null, 2000);
+
+
+        resourceManagement.findDepartmentByName("dep3", departments);
+        resourceManagement.getDepartmentWithMaxSalary(departments);
+    }
+}
---
+++ src/com/bdg/vqertikyan/resourcemanagement/ResourceManagement.java
@@ -0,0 +1,57 @@
+package com.bdg.vqertikyan.resourcemanagement;
+
+public class ResourceManagement {
+    private Department[] departments;
+
+
+    public Department findDepartmentByName(String departmentName, Department[] departments) {
+        for (int i = 0; i < departments.length; i++) {
+            if (departments[i].getDepartmentName().contentEquals(departmentName)) {
+                return departments[i];
+            }
+        }
+        return null;
+    }
+
+
+    public Department getDepartmentWithMaxSalary(Department[] departments) {
+        int maxSalary = (int) departments[0].getDepartmentSalary();
+
+        for (int i = 1; i < departments.length; i++) {
+            if (departments[i].getDepartmentSalary() > maxSalary) {
+                maxSalary = (int) departments[i].getDepartmentSalary();
+            }
+        }
+        for (int i = 1; i < departments.length; i++) {
+            if (departments[i].getDepartmentSalary() == maxSalary) {
+                return departments[i];
+            }
+        }
+        return null;
+    }
+
+
+/////if max salary not one
+
+//    public Department getDepartmentWithMaxSalary(Department[] departments) {
+//        int maxSalary = (int) departments[0].getDepartmentSalary();
+//
+//        List<Integer> maxSalaries = new ArrayList<Integer>();
+//        List<Department> departmentsWithMaxSalary = new ArrayList<Department>();
+//
+//        for (int i = 1; i < departments.length; i++) {
+//            if (departments[i].getDepartmentSalary() > maxSalary) {
+//                maxSalary = (int) departments[i].getDepartmentSalary();
+//                maxSalaries.add(maxSalary);
+//            }
+//        }
+//        for (int i = 1; i < departments.length; i++) {
+//            if (departments[i].getDepartmentSalary() == maxSalary) {
+//                departmentsWithMaxSalary.add(departments[i]);
+//               return departments[i];
+//            }
+//        }
+//        return null;
+//    }
+
+}
