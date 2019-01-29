ef3603293371f03e5f15a0e2364ac0526dbde075
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-01-29 11:44:35 +0400

    lesson 9

--- src/akarapetyan/resource_management/ResourceManagement.java
+++ src/akarapetyan/resource_management/ResourceManagement.java
@@ -9,6 +9,31 @@ public class ResourceManagement {
 
     public void addEmployeeToDepartment (Employee emp, String departmentName){
 
+        Department dep = searchDepartment(departmentName);
+        Employee [] employee_update = new Employee[dep.getEmployees().length +1];
+        for (int i = 0; i < dep.getEmployees().length; i++)
+        {
+            employee_update[i] = dep.employees[i];
+        }
+        employee_update [dep.getEmployees().length] = emp;
+        dep.employees = employee_update;
+    }
+
+    public void removeEmployeeFromDepartment (Employee emp, String departmentName){
+
+        Department dep = searchDepartment(departmentName);
+        Employee [] employee_update = new Employee[dep.getEmployees().length - 1];
+        for (int i = 0; i < dep.getEmployees().length - 1; i++) {
+
+            if (emp != dep.employees[i]) {
+                employee_update[i] = dep.employees[i];
+            }
+        }
+        dep.employees = employee_update;
+    }
+
+    public Department searchDepartment (String departmentName){
+
         Department dep = null;
 
         for (int i=0; i < departments.length; i++){
@@ -17,15 +42,22 @@ public class ResourceManagement {
             {
                 dep = departments [i];
             }
-
-        }
-        Employee [] employee_update = new Employee[dep.getEmployees().length +1];
-
-        for (int i = 0; i < dep.getEmployees().length; i++)
-        {
-            employee_update[i] = dep.employees[i];
-        }
-        employee_update [dep.getEmployees().length] = emp;
-        dep.employees = employee_update;
+        } return  dep;
     }
+
+    public Employee findEmployeeByName (String firstName, String lastName){
+
+        Department dep = null;
+        Employee emp = null;
+
+        for (int i =0; i < departments.length; i++ ) {
+            for (int m = 0; m < dep.getEmployees().length - 1; m++) {
+
+                dep.getEmployees();
+            }
+        } return emp;
+    }
+
+
+
 }
