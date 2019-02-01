dc583ca5801b4fed781989a2c375b1431f24ab59
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-01 11:43:47 +0400

    update

---
+++ src/com/bdg/akarapetyan/inheritance/Address.java
@@ -0,0 +1,14 @@
+package com.bdg.akarapetyan.inheritance;
+
+public class Address {
+
+    String city,state,country;
+
+    public Address(String city, String state, String country) {
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+}
+
+
