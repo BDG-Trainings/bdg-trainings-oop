e2a26d7b1cf550405f47745ba168bb86c0473e62
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-01 11:04:44 +0400

    update

--- src/com/bdg/akarapetyan/inheritance/Animal.java
+++ src/com/bdg/akarapetyan/inheritance/Animal.java
@@ -5,16 +5,23 @@ class Animal{
     void eat(){System.out.println("eating...");}
 }
 
-
 class Dog extends Animal{
 
     void bark(){System.out.println("barking...");}
 }
 
-class TestInheritance{
+class BabyDog extends Dog{
+
+    void weep(){System.out.println("weeping...");}
+}
+
+class TestInheritance2{
 
     public static void main(String args[]){
-        Dog d = new Dog();
+
+        BabyDog d=new BabyDog();
+
+        d.weep();
         d.bark();
         d.eat();
     }
