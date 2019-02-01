f9a8e1c8a60c143edd82b5fcf49bd57de314dbc5
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-01 11:18:38 +0400

    update

--- src/com/bdg/akarapetyan/inheritance/Animal.java
+++ src/com/bdg/akarapetyan/inheritance/Animal.java
@@ -19,7 +19,7 @@ class TestInheritance2{
 
     public static void main(String args[]){
 
-        BabyDog d=new BabyDog();
+        BabyDog d = new BabyDog();
 
         d.weep();
         d.bark();
