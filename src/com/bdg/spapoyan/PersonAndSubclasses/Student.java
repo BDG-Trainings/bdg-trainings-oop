40c9ef7103ad81e0a581f2697956b5b6199dab33
Sergey Papoyan <spapoyan87@gmail.com>
2019-02-03 20:09:09 +0400

    new_bdg_class

--- src/com/bdg/spapoyan/PersonAndSubclasses/Student.java
+++ src/com/bdg/spapoyan/PersonAndSubclasses/Student.java
@@ -2,8 +2,27 @@ package com.bdg.spapoyan.PersonAndSubclasses;
 
 public class Student extends Person {
 
-    public Student(String name, String address) {
+    private String program;
+    private int year;
+    private double fee;
+
+    public Student(final String name, final String address, final String program, final int year, final double fee) {
         super(name, address);
+        this.program = program;
+        this.year = year;
+        this.fee = fee;
+    }
+    public String getProgram() { return program; }
+    public void setProgram(String program) { this.program = program; }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+    public double getFee() { return fee; }
+    public void setFee(double fee) { this.fee = fee; }
+
+    @Override
+    public String toString(){
+        return "Student[Person[name = " + getName() + ", " + "address = " + getAddress() + "],"
+                + "program = " + getProgram() + ", " + "year = " + getYear() + ", " + "fee = " + getFee() + "]";
     }
 }
 
