fe185f2123c92aca1fe586a2c69391e1db80c2de
Arsen Karapetyan <arsen.karapetyansam@gmail.com>
2019-02-06 18:58:09 +0400

    update

--- src/com/bdg/akarapetyan/Lesson_10/App_classroom.java
+++ src/com/bdg/akarapetyan/Lesson_10/App_classroom.java
@@ -4,15 +4,8 @@ public class App_classroom {
 
     private final int x;
     public static final int DEFAULT_VALUE = 20;
-
-    {
-        x=30;
-    }
-
-    public App_classroom(){
-
-    }
-
+    {x=30;}
+    public App_classroom(){}
     public static void main(String[] args) {
         System.out.println(DEFAULT_VALUE);
     }
