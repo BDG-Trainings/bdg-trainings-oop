44ab4d4a07d1d4f85a5c5c5ebd4b0b2bd4d8b342
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-08 09:19:19 +0400

    update

---
+++ src/com/bdg/akarapetyan/inheritance_homework/Executor.java
@@ -0,0 +1,10 @@
+package com.bdg.akarapetyan.inheritance_homework;
+
+public class Executor {
+
+    public static void main(String[] args) {
+
+        Cylinder c = new Cylinder();
+        System.out.println(c.getArea());
+    }
+}
