40abe7b6a3c371d1066def89f6f7a9d7b78b0fb4
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-08 15:27:17 +0400

    update

--- src/com/bdg/akarapetyan/inheritance_homework/Point2D.java
+++ src/com/bdg/akarapetyan/inheritance_homework/Point2D.java
@@ -5,7 +5,39 @@ public class Point2D {
     private float x = 0.0f;
     private float y = 0.0f;
 
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 
+    public Point2D() {
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float [] getXY() {
+        float [] array = {x,y};
+        return array;
+    }
+
+    public void setXY(float y, float x) {
+        this.y = y;
+        this.x = x;
+    }
 
 
 }
