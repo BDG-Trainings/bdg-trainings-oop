4edc68981581feaeb1e5b0a34c1d8f3c96438eaf
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-08 15:32:06 +0400

    update

--- src/com/bdg/akarapetyan/inheritance_homework/Point2D.java
+++ src/com/bdg/akarapetyan/inheritance_homework/Point2D.java
@@ -38,6 +38,4 @@ public class Point2D {
         this.y = y;
         this.x = x;
     }
-
-
 }
---
+++ src/com/bdg/akarapetyan/inheritance_homework/Point3D.java
@@ -0,0 +1,8 @@
+package com.bdg.akarapetyan.inheritance_homework;
+
+public class Point3D {
+
+    private float z = 0.0f;
+
+
+}
