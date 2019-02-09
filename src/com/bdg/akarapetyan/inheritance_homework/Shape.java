9a696258eceb0395f2e134942021367e80fb7bd2
Arsen Karapetyan <arsen.karapetyansam@gmail.com>
2019-02-09 12:44:31 +0400

    update

--- src/com/bdg/akarapetyan/inheritance_homework/Shape.java
+++ src/com/bdg/akarapetyan/inheritance_homework/Shape.java
@@ -5,9 +5,11 @@ public class Shape {
     private String color = "red";
     private boolean filled = true;
 
+    public Shape() {
+    }
 
-
-
-
-
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
 }
