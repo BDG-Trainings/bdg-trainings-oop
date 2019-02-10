7648e92538c1108bd3d08f2e4ff43e86603f16ba
Varduhi <varduhi.qertikyan@gmail.com>
2019-02-10 14:59:40 +0400

    bookstorage

---
+++ src/com/bdg/vqertikyan/bookstorage/Author.java
@@ -0,0 +1,48 @@
+package com.bdg.vqertikyan.bookstorage;
+
+public class Author {
+
+    private int id;
+    private String name;
+    private String surName;
+    private String gender;
+
+    public Author(int id, String name, String surName, String gender){
+        this.id = id;
+        this.name = name;
+        this.surName = surName;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/com/bdg/vqertikyan/bookstorage/Book.java
@@ -0,0 +1,60 @@
+package com.bdg.vqertikyan.bookstorage;
+
+import com.bdg.vqertikyan.bookstorage.Author;
+
+public class Book {
+
+    private int id;
+    private String name;
+    private String title;
+    private double price;
+    private Author[] authors;
+
+    public Book(int id, String name, String title, double price, Author[] authors){
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.price = price;
+        this.authors = authors;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Author[] authors) {
+        this.authors = authors;
+    }
+}
