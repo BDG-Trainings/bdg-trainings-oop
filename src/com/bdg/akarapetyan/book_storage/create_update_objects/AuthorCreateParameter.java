84eaa1565b3f27ec188ef5625f8002b7bf20270c
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-12 15:15:50 +0400

    update

---
+++ src/com/bdg/akarapetyan/book_storage/create_update_objects/AuthorCreateParameter.java
@@ -0,0 +1,38 @@
+package com.bdg.akarapetyan.book_storage.create_update_objects;
+
+public class AuthorCreateParameter {
+
+    private String name;
+    private String surname;
+    private String gender;
+
+    public AuthorCreateParameter(String name, String surname, String gender) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/com/bdg/akarapetyan/book_storage/create_update_objects/BookCreateParameter.java
@@ -0,0 +1,10 @@
+package com.bdg.akarapetyan.book_storage.create_update_objects;
+
+public class BookCreateParameter {
+
+    private String name;
+    private String title;
+    private double price;
+
+
+}
