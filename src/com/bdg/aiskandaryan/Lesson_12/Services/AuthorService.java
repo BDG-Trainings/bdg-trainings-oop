752ae3eb4d1a52e4dadaf31abda54e807ad3f195
Home <armenir@rambler.ru>
2019-02-13 01:42:45 +0400

    homework #12

---
+++ src/com/bdg/aiskandaryan/Lesson_12/Services/AuthorService.java
@@ -0,0 +1,36 @@
+package com.bdg.aiskandaryan.Lesson_12.Services;
+
+import com.bdg.aiskandaryan.Lesson_12.Author;
+import com.bdg.aiskandaryan.Lesson_12.Services.CreateUpdateObjects.AuthorCreateParameter;
+import com.bdg.aiskandaryan.Lesson_12.Services.CreateUpdateObjects.AuthorUpdateParameter;
+import com.bdg.aiskandaryan.Lesson_12.Storages.AuthorStorage;
+
+public class AuthorService extends AbstractAuthorService {
+    private AuthorStorage authorStorage;
+
+    public AuthorService(final AuthorStorage authorStorage) {
+        this.authorStorage = authorStorage;
+    }
+
+    @Override
+    public Author get(final int id) {
+        return authorStorage.get(id);
+    }
+
+    @Override
+    public Author create(final AuthorCreateParameter params) {
+        Author author = new Author(100 /**/, params.getName(), params.getSurname(), params.isGender());
+        return authorStorage.store(author);
+    }
+
+    @Override
+    public Author update(final AuthorUpdateParameter params) {
+        Author author = new Author(100 /**/, params.getName(), params.getSurname(), params.isGender());
+        return null; ////***********
+    }
+
+    @Override
+    public boolean delete(final int id) {
+        return authorStorage.remove(authorStorage.get(id));
+    }
+}
--- src/com/bdg/aiskandaryan/Lesson_12/Services/CreateUpdateObjects/AuthorUpdateParameter.java
+++ src/com/bdg/aiskandaryan/Lesson_12/Services/CreateUpdateObjects/AuthorUpdateParameter.java
@@ -4,10 +4,28 @@ public class AuthorUpdateParameter {
     private int id;
     private String name;
     private String surname;
+    private boolean gender;
 
-    public AuthorUpdateParameter(final int id, final String name, final String surname) {
+    public AuthorUpdateParameter(final int id, final String name, final String surname, final boolean gender) {
         this.id = id;
         this.name = name;
         this.surname = surname;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public boolean isGender() {
+        return gender;
     }
 }
