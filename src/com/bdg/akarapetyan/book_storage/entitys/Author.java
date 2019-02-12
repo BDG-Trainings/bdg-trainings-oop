5e88db42b2c1afc36f3de23aeef88a6d0b7687a3
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-12 14:58:29 +0400

    update

--- src/com/bdg/akarapetyan/book_storage/entitys/Author.java
+++ src/com/bdg/akarapetyan/book_storage/entitys/Author.java
@@ -2,6 +2,39 @@ package com.bdg.akarapetyan.book_storage.entitys;
 
 public class Author {
 
+    private int id;
     private String name;
+    private String surname;
+    private boolean gender;
 
+    public Author(int id, String name, String surname, boolean gender) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
 }
