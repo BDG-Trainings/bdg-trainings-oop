67dbe0f0b757cbf8b59668edcaf17b5757156422
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-13 10:58:55 +0400

    update

---
+++ src/com/bdg/akarapetyan/book_storage/services/AuthorService.java
@@ -0,0 +1,28 @@
+package com.bdg.akarapetyan.book_storage.services;
+
+import com.bdg.akarapetyan.book_storage.create_update_objects.AuthorCreateParameter;
+import com.bdg.akarapetyan.book_storage.create_update_objects.BookUpdateParameter;
+import com.bdg.akarapetyan.book_storage.entitys.Author;
+
+public class AuthorService extends AbstractAuthorService{
+
+    @Override
+    public Author get(int id) {
+        return null;
+    }
+
+    @Override
+    public Author create(AuthorCreateParameter params) {
+        return null;
+    }
+
+    @Override
+    public Author update(BookUpdateParameter params) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+}
