90051c30194fdaf340c53f7161599cf0804c77b2
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-13 10:08:08 +0400

    update

--- src/com/bdg/akarapetyan/book_storage/create_update_objects/BookUpdateParameter.java
+++ src/com/bdg/akarapetyan/book_storage/create_update_objects/BookUpdateParameter.java
@@ -5,6 +5,24 @@ public class BookUpdateParameter {
     private int id;
     private double price;
 
+    public BookUpdateParameter(int id, double price) {
+        this.id = id;
+        this.price = price;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
