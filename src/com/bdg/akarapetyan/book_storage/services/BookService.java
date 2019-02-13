e98a3369340e0a8a2f197551922251088dcbe950
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-13 12:44:15 +0400

    update

--- src/com/bdg/akarapetyan/book_storage/services/BookService.java
+++ src/com/bdg/akarapetyan/book_storage/services/BookService.java
@@ -3,15 +3,21 @@ package com.bdg.akarapetyan.book_storage.services;
 import com.bdg.akarapetyan.book_storage.create_update_objects.BookCreateParameter;
 import com.bdg.akarapetyan.book_storage.create_update_objects.BookUpdateParameter;
 import com.bdg.akarapetyan.book_storage.entitys.Book;
+import com.bdg.akarapetyan.book_storage.storage.BookStorage;
 
 public class BookService extends AbstractBookService {
 
-    public BookService() {
+    private BookStorage storage;
+
+    private AuthorService authorService = new AuthorService();
+
+    public BookService(BookStorage storage) {
+        this.storage = storage;
     }
 
     @Override
     public Book get(int id) {
-        return null;
+        return storage.get(id);
     }
 
     @Override
