d86d80a2a058952d3edec750ac8e39e2621f8297
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-13 12:29:15 +0400

    update

---
+++ src/com/bdg/akarapetyan/book_storage/storage/BookStorage.java
@@ -0,0 +1,27 @@
+package com.bdg.akarapetyan.book_storage.storage;
+
+import com.bdg.akarapetyan.book_storage.entitys.Book;
+
+public class BookStorage extends AbstractBookStorage{
+
+    private int currentStorageSize;
+
+    public BookStorage(int currentStorageSize) {
+        this.currentStorageSize = currentStorageSize;
+    }
+
+    @Override
+    public Book store(Book book) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Book book) {
+        return false;
+    }
+
+    @Override
+    public Book get(int id) {
+        return null;
+    }
+}
