c64c68c7f3a557b4aaeb01d4188ec77855f2baf7
Vika <vika.karamyan@gmail.com>
2019-02-17 00:23:07 +0400

    Done country class

--- src/com/bdg/vkaramyan/bas/Country.java
+++ src/com/bdg/vkaramyan/bas/Country.java
@@ -1,50 +1,47 @@
 package com.bdg.vkaramyan.bas;
 
 public enum Country {
-	
-	AF("Afghanistan", "004"),
-    AM("Armenia", "051"),
-    FR("France", "250"),
-    IT("Italy", "380"),
-    US("United States of America", "840");
 
-    private final String countryName;
-    private final String numericCode;
+	AF("Afghanistan", "004"), AM("Armenia", "051"), FR("France", "250"), IT("Italy",
+			"380"), US("United States of America", "840");
 
-    Country(final String countryName, final String numericCode) {
-        this.countryName = countryName;
-        this.numericCode = numericCode;
-    }
+	private final String countryName;
+	private final String numericCode;
 
-    public String getCountryName() {
-        return this.countryName;
-    }
+	Country(final String countryName, final String numericCode) {
+		this.countryName = countryName;
+		this.numericCode = numericCode;
+	}
 
-    public String getNumericCode() {
-        return this.numericCode;
-    }
-    
-  //Implement this functionality
-    public static Country findByCountryName(final String countryName) {
-    	Country [] countries = values();
-    	for(int i = 0;  i < countries.length; i++) {
-    		if (countries[i].getCountryName() == countryName) {
-    			return countries[i];
-    		}
-    	}
-        return null;
-    }
+	public String getCountryName() {
+		return this.countryName;
+	}
 
-    //Implement this functionality
-    public static Country findByNumericCode(final String numericCode) {
-    	Country[] countries = values();
-    	for (int i = 0; i < countries.length; i++) {
-    		if(countries[i].getNumericCode() == numericCode) {
-    			return countries[i];
-    		}
-    	}
-        
-        return null;
-    }
+	public String getNumericCode() {
+		return this.numericCode;
+	}
+
+	// Implement this functionality
+	public static Country findByCountryName(final String countryName) {
+		Country[] countries = values();
+		for (int i = 0; i < countries.length; i++) {
+			if (countries[i].getCountryName() == countryName) {
+				return countries[i];
+			}
+		}
+		return null;
+	}
+
+	// Implement this functionality
+	public static Country findByNumericCode(final String numericCode) {
+		Country[] countries = values();
+		for (int i = 0; i < countries.length; i++) {
+			if (countries[i].getNumericCode() == numericCode) {
+				return countries[i];
+			}
+		}
+
+		return null;
+	}
 
 }
