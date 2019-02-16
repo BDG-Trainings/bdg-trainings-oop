a4f16798edd47eb16741c59df51081a2649041de
Arsen Karapetyan <arsen.karapetyansam@gmail.com>
2019-02-16 13:59:47 +0400

    update

--- src/com/bdg/akarapetyan/bas/Account.java
+++ src/com/bdg/akarapetyan/bas/Account.java
@@ -6,7 +6,24 @@ public final class Account {
     private AccountBalance accountBalance;
 
     public Account(final Customer customer, final AccountBalance balance) {
+        this.customer = customer;
+        this.accountBalance = balance;
+    }
 
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public AccountBalance getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(AccountBalance accountBalance) {
+        this.accountBalance = accountBalance;
     }
 
     public void deposit(final double amount) {
