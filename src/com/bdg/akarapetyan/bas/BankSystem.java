da399b8673948aae3820e7b482609cf0207726dc
Arsen Karapetyan <arsen.karapetyansam@gmail.com>
2019-02-16 13:49:03 +0400

    update

--- src/com/bdg/akarapetyan/bas/BankSystem.java
+++ src/com/bdg/akarapetyan/bas/BankSystem.java
@@ -3,18 +3,17 @@ package com.bdg.akarapetyan.bas;
 public final class BankSystem {
 
     public static void main(String[] args) {
-        BankService service = new BankService(10);
+        //BankService service = new BankService(10);
         //Add missing functionality
 
-        Address address = new Address(Country.IT, "Moscovyan", "094566747" );
-        System.out.println(address);
+        //Address address = new Address(Country.IT, "Moscovyan", "094566747" );
+        //System.out.println(address);
 
-        Customer customer = new Customer(1, "Arsen", "Karapetyan", address );
-        System.out.println(customer);
+        //Customer customer = new Customer(1, "Arsen", "Karapetyan", address );
+        //System.out.println(customer);
 
-        Country country = Country.AM;
-        System.out.println(country.getCountryName());
-        System.out.println(country.getNumericCode());
+        System.out.println(Country.findByCountryName("Asajdjadasj"));
+        System.out.println(Country.findByNumericCode("051"));
     }
 
 }
