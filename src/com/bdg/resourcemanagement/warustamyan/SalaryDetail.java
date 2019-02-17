d9621dac124a7ef439629a9d29f8c45e04c7301f
William Arustamyan <williamarustamyan@gmail.com>
2019-02-17 15:59:16 +0400

    Added SalaryDetail class

---
+++ src/com/bdg/resourcemanagement/warustamyan/SalaryDetail.java
@@ -0,0 +1,32 @@
+package com.bdg.resourcemanagement.warustamyan;
+
+public final class SalaryDetail {
+
+    private double salaryAmount;
+    private SalaryType salaryType;
+    private String cardNumber;
+
+    public SalaryDetail(final double amount, String cardNumber) {
+        this.salaryAmount = amount;
+        this.cardNumber = cardNumber;
+        this.salaryType = SalaryType.CARD;
+    }
+
+    public SalaryDetail(final double amount) {
+        this.salaryAmount = amount;
+        this.salaryType = SalaryType.CASH;
+        this.cardNumber = null;
+    }
+
+    public double getSalaryAmount() {
+        return salaryAmount;
+    }
+
+    public SalaryType getSalaryType() {
+        return salaryType;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+}
