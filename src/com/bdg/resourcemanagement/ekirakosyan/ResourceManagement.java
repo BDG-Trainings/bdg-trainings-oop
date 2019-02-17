eca49e969eadc43e3eaf16dbb593dde81ee5a25f
elmira_k82 <elmirakirakosyan@gmail.com>
2019-02-17 17:52:50 +0400

    added ResourceManagement class

---
+++ src/com/bdg/resourcemanagement/ekirakosyan/ResourceManagement.java
@@ -0,0 +1,23 @@
+package com.bdg.resourcemanagement.ekirakosyan;
+
+public class ResourceManagement {
+
+    private Department[] departments;
+    private int currentIndex;
+
+    public ResourceManagement(final int departmentSize) {
+        this.departments = new Department[departmentSize];
+    }
+
+    public boolean addDepartment(final Department department) {
+        if(currentIndex < this.departments.length){
+            this.departments[currentIndex] = department;
+            currentIndex++;
+            return true;
+        }
+        System.out.println("Departments is full");
+        return false;
+    }
+
+
+}
