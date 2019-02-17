dbfbc3ce669a5563422bbe32d369c8a4cc0245ec
Sergey Papoyan <spapoyan87@gmail.com>
2019-02-17 18:52:51 +0400

    Application

--- src/com/bdg/resourcemanagement/spapoyan/Application.java
+++ src/com/bdg/resourcemanagement/spapoyan/Application.java
@@ -3,7 +3,6 @@ package com.bdg.resourcemanagement.spapoyan;
 public class Application {
     public static void main(String[] args) {
 
-        ResourceManagement management = new ResourceManagement(10);
 
     }
 }
