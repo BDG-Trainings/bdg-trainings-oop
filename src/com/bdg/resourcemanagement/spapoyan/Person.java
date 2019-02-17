3b1a97d327da8b9ed4f0761d5beaa875546bbb57
Sergey Papoyan <spapoyan87@gmail.com>
2019-02-17 14:25:00 +0400

    Person class

---
+++ src/com/bdg/resourcemanagement/spapoyan/Person.java
@@ -0,0 +1,48 @@
+package com.bdg.resourcemanagement.spapoyan;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private short age;
+
+    public Person(String firstName, String lastName, String middleName, short age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public short getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
+}
