7ad5db0444f52a89ec9e9c212ced40df73c29c79
Vika <vika.karamyan@gmail.com>
2019-02-17 15:00:55 +0400

    Push country class

---
+++ src/com/bdg/vkaramyan/resourceManagement/Country.java
@@ -0,0 +1,26 @@
+package com.bdg.vkaramyan.resourceManagement;
+
+public enum Country {
+	AM("Armenia", "Yerevan"),
+    RU("Russia", "Moscow"),
+    US("USA", "Washington");
+
+
+    private final String countryName;
+    private final String city;
+
+    Country(String countryName, String city) {
+        this.countryName = countryName;
+        this.city = city;
+    }
+
+    public String getCountryName() {
+        return this.countryName;
+    }
+
+    public String getCity() {
+        return this.city;
+   
+    }
+
+}
