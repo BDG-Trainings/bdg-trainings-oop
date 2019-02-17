053888496df2017c8f114d616d652cfeca68e756
Sergey Papoyan <spapoyan87@gmail.com>
2019-02-17 14:58:31 +0400

    enum Country

--- src/com/bdg/resourcemanagement/spapoyan/Country.java
+++ src/com/bdg/resourcemanagement/spapoyan/Country.java
@@ -2,7 +2,27 @@ package com.bdg.resourcemanagement.spapoyan;
 
 public enum Country {
 
-    AM ("Armenia", "Yerevan");
+
+    AM("Armenia", "Yerevan"),
+    RU("Russia", "Moscow"),
+    US("USA", "Washington");
 
 
+    private final String countryName;
+    private final String city;
+
+    Country(String countryName, String city) {
+        this.countryName = countryName;
+        this.city = city;
+    }
+
+    public String getCountryName() {
+        return this.countryName;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
 }
+
+
