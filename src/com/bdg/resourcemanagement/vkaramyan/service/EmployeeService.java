a70add57fd4e321801ab6bfd060f7c431e45a498
Vika <vika.karamyan@gmail.com>
2019-02-17 17:18:23 +0400

    Pushed Employee Service

--- src/com/bdg/resourcemanagement/vkaramyan/service/EmployeeService.java
+++ src/com/bdg/resourcemanagement/vkaramyan/service/EmployeeService.java
@@ -1,5 +1,17 @@
 package com.bdg.resourcemanagement.vkaramyan.service;
 
-public class EmployeeService {
+import com.bdg.resourcemanagement.vkaramyan.Employee;
+import com.bdg.resourcemanagement.vkaramyan.Person;
+import com.bdg.resourcemanagement.vkaramyan.SalaryDetail;
 
+//constructori anhrajeshtutyun chka qani vor ogtagorcelu enq service-i create methody vory stanaluya parameternery new ani u het veradarcni
+
+public class EmployeeService {
+	
+	public Employee create(final Person person, final SalaryDetail salaryDetail){
+        Employee employee = new Employee(salaryDetail, person);
+        return employee;
+        
+	}
+	
 }
