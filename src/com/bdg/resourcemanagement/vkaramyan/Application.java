b8664a6ccff0a9ad973559d4e525fac8f1b76513
Vika <vika.karamyan@gmail.com>
2019-02-17 18:01:49 +0400

    Push Application class

---
+++ src/com/bdg/resourcemanagement/vkaramyan/Application.java
@@ -0,0 +1,10 @@
+package com.bdg.resourcemanagement.vkaramyan;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		ResourceManagement resourceManagement = new ResourceManagement(10);
+		
+	}
+
+}
