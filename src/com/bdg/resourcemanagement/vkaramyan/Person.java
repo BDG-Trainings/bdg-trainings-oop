0e6c641539590d204b54ed022bc3b5524bc3e058
Vika <vika.karamyan@gmail.com>
2019-02-17 16:08:31 +0400

    Address and Person class update

--- src/com/bdg/resourcemanagement/vkaramyan/Person.java
+++ src/com/bdg/resourcemanagement/vkaramyan/Person.java
@@ -1,18 +1,22 @@
 package com.bdg.resourcemanagement.vkaramyan;
 
+import java.util.Arrays;
+
 public class Person {
 
 	private String firstName;
 	private String lastName;
 	private String middleName;
 	private short age;
+	private Address [] address;
 
-	public Person(final String firstName, final String lastName, final String middleName, final short age) {
+	public Person(final String firstName, final String lastName, final String middleName, final short age, final Address [] address) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.middleName = middleName;
 		this.age = age;
+		this.address = address;
 	}
 
 	public String getFirstName() {
@@ -30,12 +34,22 @@ public class Person {
 
 	public short getAge() {
 		return age;
+		
+	}
+	
+	
+
+	public Address[] getAddress() {
+		return address;
 	}
 
 	@Override
 	public String toString() {
 		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", middleName=" + middleName + ", age="
-				+ age + "]";
+				+ age + ", address=" + Arrays.toString(address) + "]";
 	}
+
+	
+	
 
 }
