d30aa89bae22e1396ffd065a72e48bcbf8689ea2
Vika <vika.karamyan@gmail.com>
2019-02-17 15:09:09 +0400

    Done address, country and person classes

---
+++ src/com/bdg/resourcemanagement/vkaramyan/Person.java
@@ -0,0 +1,41 @@
+package com.bdg.vkaramyan.resourceManagement;
+
+public class Person {
+	
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private short age;
+	
+	public Person(final String firstName, final String lastName, final String middleName, final short age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.middleName = middleName;
+		this.age = age;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public short getAge() {
+		return age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", middleName=" + middleName + ", age="
+				+ age + "]";
+	}
+
+}
