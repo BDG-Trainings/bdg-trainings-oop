2395851afc4f158bc5b17d06c51d9f0cc2f45905
elmira_k82 <elmirakirakosyan@gmail.com>
2019-02-17 17:05:37 +0400

    removed EmployeeService constructor and fields

--- src/com/bdg/resourcemanagement/ekirakosyan/service/EmployeeService.java
+++ src/com/bdg/resourcemanagement/ekirakosyan/service/EmployeeService.java
@@ -6,14 +6,6 @@ import com.bdg.resourcemanagement.ekirakosyan.SalaryDetail;
 
 public class EmployeeService {
 
-    private SalaryDetail salaryDetail;
-    private Person person;
-
-    public EmployeeService(SalaryDetail salaryDetail, Person person) {
-        this.salaryDetail = salaryDetail;
-        this.person = person;
-    }
-
     public Employee create(SalaryDetail salaryDetail, Person person){
         Employee employee = new Employee(salaryDetail, person);
         return employee;
