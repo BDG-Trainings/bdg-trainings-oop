686e6027c788ef5e27c34cde1fe29137fe85099a
Sergey Papoyan <spapoyan87@gmail.com>
2019-02-17 12:40:03 +0400

    bas.account

--- src/com/bdg/spapoyan/bas/Account.java
+++ src/com/bdg/spapoyan/bas/Account.java
@@ -7,15 +7,26 @@ public final class Account {
     private AccountBalance accountBalance;
 
     public Account(final Customer customer, final AccountBalance balance) {
+        this.customer = customer;
+        this.accountBalance = balance;
 
     }
 
     public void deposit(final double amount) {
-
+        boolean success = false;
+        if(this.accountBalance.getBalance()+ amount == accountBalance.getBalance()) {
+            this.accountBalance.deposit(amount);
+            success = true;
+        }
     }
 
     public boolean withdraw(final double amount) {
-        return false;
+        boolean success = false;
+        if(this.accountBalance.getBalance()>=amount){
+            this.accountBalance.withdraw(amount);
+            success = true;
+        }
+        return success;
     }
 }
 
