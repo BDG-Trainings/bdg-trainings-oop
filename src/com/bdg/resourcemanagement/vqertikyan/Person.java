d690c6de576be2d05d71622e8af03df1e4d98897
Varduhi <varduhi.qertikyan@gmail.com>
2019-02-17 14:38:02 +0400

    Person

--- src/com/bdg/resourcemanagement/vqertikyan/Person.java
+++ src/com/bdg/resourcemanagement/vqertikyan/Person.java
@@ -6,7 +6,7 @@ public class Person {
     private String middleName;
     private short age;
 
-    public Person(String firstName, String lastName, String middleName, short age){
+    public Person(final String firstName, final String lastName, final String middleName,final short age){
         this.firstName = firstName;
         this.lastName = lastName;
         this.middleName = middleName;
