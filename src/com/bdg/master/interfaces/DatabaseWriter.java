3f11a11117f29c4ff1827017d3515f98610c329b
William Arustamyan <williamarustamyan@workfront.com>
2019-02-20 21:47:48 +0400

    interfaces

---
+++ src/com/bdg/master/interfaces/DatabaseWriter.java
@@ -0,0 +1,10 @@
+package com.bdg.master.interfaces;
+
+
+public class DatabaseWriter {
+
+
+    public DatabaseWriter(DatabaseConnector databaseConnector) {
+    }
+
+}
---
+++ src/com/bdg/master/interfaces/MySQLConnector.java
@@ -0,0 +1,23 @@
+package com.bdg.master.interfaces;
+
+
+public class MySQLConnector implements DatabaseConnector, FileSystemConnector {
+
+
+    @Override
+    public Connection openConnection() {
+        System.out.println("Connecting to database ");
+        return null;
+    }
+
+    @Override
+    public boolean closeConnection() {
+        return false;
+    }
+
+    @Override
+    public Connection getFileSystemConnector() {
+        System.out.println("Connecting to file system");
+        return null;
+    }
+}
