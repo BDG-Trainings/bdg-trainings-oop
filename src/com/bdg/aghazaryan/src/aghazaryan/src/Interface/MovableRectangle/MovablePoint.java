3949c28519f0d0705b40282db6fa085af0417f03
Anahit <anahit.ghazaryan91@gmail.com>
2019-02-23 21:24:03 +0400

    some updates of MovableRectangle

--- src/com/bdg/aghazaryan/src/aghazaryan/src/Interface/MovableRectangle/MovablePoint.java
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/Interface/MovableRectangle/MovablePoint.java
@@ -4,30 +4,37 @@ import com.bdg.aghazaryan.src.aghazaryan.src.Interface.Movable.Movable;
 
 public class MovablePoint implements Movable {
 
-
-    public int x1, int y1, int x2, int y2, int xSpeed, int ySpeed;
+    int x1, y1, x2, y2;
+            double xSpeed, ySpeed;
 
     public MovablePoint(int x1, int y1, int x2, int y2, int xSpeed, int ySpeed) {
         this.x1 = x1;
         this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
     }
 
     public void moveUp() {
         y1 -= ySpeed;
+        y2 -= ySpeed;
     }
 
     public void moveDown() {
         y1 += ySpeed;
+        y2 += ySpeed;
 
     }
 
     public void moveLeft() {
         x1 -= xSpeed;
+        x2 -= xSpeed;
     }
 
     public void moveRight() {
         x1 += xSpeed;
+        x2 += xSpeed;
+
     }
 }
--- src/com/bdg/aghazaryan/src/aghazaryan/src/Interface/MovableRectangle/MovableRectangle.java
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/Interface/MovableRectangle/MovableRectangle.java
@@ -5,9 +5,50 @@ public class MovableRectangle extends MovablePoint implements Movable {
     MovablePoint topLeft;
     MovablePoint bottomRight;
 
-    public MovableRectangle(int x1, int y1, int x2, int y2, int xSpeed, int ySpeed) {
-        this.
+    public MovableRectangle(int x1, int y1, int x2, int y2, int xSpeed, int ySpeed, MovablePoint topLeft,
+                            MovablePoint bottomRight) {
+        super(x1, y1, x2, y2, xSpeed, ySpeed);
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+
+        topLeft = new MovablePoint (5, 2, 6, 5, 6,5);
+        bottomRight = new MovablePoint (5,2,6, 5,6,5);
+
+    }
+
+    @Override
+    public String toString() {
+        return "MovableRectangle [topLeft=" + topLeft + ", bottomRight=" + bottomRight + "]";
+    }
+
+    @Override
+    public void moveUp() {
+        super.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        super.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        super.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        super.moveRight();
+    }
+
+    public static void main(String args[]) {
+        MovableRectangle rectangle = new MovableRectangle(6,5,8,9,10,8, 5,5);
+        System.out.println(rectangle.topLeft);
+        System.out.println(rectangle.bottomRight);
+
+
 
     }
 
 }
+
