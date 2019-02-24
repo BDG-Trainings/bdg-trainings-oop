a843aa024db73af418f63c254a272c2a614e3c02
narekp92 <npetrosyan1991@gmail.com>
2019-02-24 16:48:15 +0400

    update1

---
+++ src/com/bdg/bookstorage/npetrosyan/entity/Book.java
@@ -0,0 +1,57 @@
+package com.bdg.bookstorage.npetrosyan.entity;
+
+public class Book {
+    private int id;
+    private String name;
+    private String title;
+    private double price;
+    private Author authors;
+
+    public Book(final int id, final String name, final String title,final double price, final Author authors){
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.price = price;
+        this.authors = authors;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return authors;
+    }
+
+    public void setAuthor(Author authors) {
+        this.authors = authors;
+    }
+
+}
