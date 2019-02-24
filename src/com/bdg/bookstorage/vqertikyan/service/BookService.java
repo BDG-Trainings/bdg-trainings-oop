c697319b8a2f1da155d8abb4d38b789bf7d2fd15
Varduhi <varduhi.qertikyan@gmail.com>
2019-02-24 17:08:21 +0400

    added BookServices

--- src/com/bdg/bookstorage/vqertikyan/service/BookService.java
+++ src/com/bdg/bookstorage/vqertikyan/service/BookService.java
@@ -1,8 +1,8 @@
 package com.bdg.bookstorage.vqertikyan.service;
-
-import com.bdg.bookstorage.aghazaryan.entity.Book;
+import com.bdg.bookstorage.vqertikyan.entity.Book;
 
 public interface BookService {
+
     Book create();
     Book update();
     Book delete();
