2fe5e9c7e1db4f34f275e927405d750bbd0afb98
Sergey Papoyan <spapoyan87@gmail.com>
2019-02-24 17:10:26 +0400

    service

--- src/com/bdg/bookstorage/spapoyan/service/BookService.java
+++ src/com/bdg/bookstorage/spapoyan/service/BookService.java
@@ -1,13 +1,10 @@
 package com.bdg.bookstorage.spapoyan.service;
 
-import com.bdg.bookstorage.aghazaryan.entity.Book;
-
 public interface BookService {
 
-    Book get();
-    Book create();
-    Book update();
-    Book delete();
-
+    BookService get();
+    BookService create();
+    BookService update();
+    BookService delete();
 
 }
