7e75cbcba122a001634b8295dd5006e773c4a343
William Arustamyan <williamarustamyan@gmail.com>
2019-02-24 18:41:46 +0400

    Refactoring

--- src/com/bdg/bookstorage/aghazaryan/service/BookServiceImpl.java
+++ src/com/bdg/bookstorage/aghazaryan/service/BookServiceImpl.java
@@ -1,21 +1,41 @@
 package com.bdg.bookstorage.aghazaryan.service;
 
+import com.bdg.bookstorage.aghazaryan.common.AuthorCreateParameter;
 import com.bdg.bookstorage.aghazaryan.common.BookCreateParameter;
 import com.bdg.bookstorage.aghazaryan.common.BookUpdateParameter;
+import com.bdg.bookstorage.aghazaryan.entity.Author;
 import com.bdg.bookstorage.aghazaryan.entity.Book;
 
 public class BookServiceImpl implements BookService {
 
 
+    private AuthorService authorService;
+
+    public BookServiceImpl() {
+        this.authorService = new AuthorServiceImpl();
+    }
 
 
     @Override
-    public Book create(BookCreateParameter createParameter) {
+    public Book create(final BookCreateParameter createParameter) {
+
+        final Author[] authors = new Author[createParameter.getAuthorCreateParameters().length];
+
+        int i = 0;
+        for (AuthorCreateParameter p : createParameter.getAuthorCreateParameters()) {
+            authors[i] = this.authorService.create(p);
+            i++;
+        }
+
+
+        final Book book = new Book(1, createParameter.getName(), createParameter.getPrice(), authors);
+
+
         return null;
     }
 
     @Override
-    public Book update(BookUpdateParameter createParameter) {
+    public Book update(final BookUpdateParameter updateParameter) {
         return null;
     }
 
