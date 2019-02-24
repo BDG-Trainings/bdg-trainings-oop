b589e09467e5132d039bd7be094b4ff0021bb386
Anahit <anahit.ghazaryan91@gmail.com>
2019-02-24 17:38:53 +0400

    BookCreateParameter update

--- src/com/bdg/bookstorage/aghazaryan/common/BookCreateParameter.java
+++ src/com/bdg/bookstorage/aghazaryan/common/BookCreateParameter.java
@@ -3,12 +3,10 @@ package com.bdg.bookstorage.aghazaryan.common;
 public class BookCreateParameter {
 
     private String name;
-    private String title;
     private int price;
 
     public BookCreateParameter(String name, String title, int price) {
         this.name = name;
-        this.title = title;
         this.price = price;
     }
 
@@ -16,19 +14,14 @@ public class BookCreateParameter {
         return name;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public int getPrice() {
+      public int getPrice() {
         return price;
     }
 
     @Override
     public String toString() {
         return "BookCreateParameter{" +
-                ", name='" + name + '\'' +
-                ", title='" + title + '\'' +
+                "name='" + name + '\'' +
                 ", price=" + price +
                 '}';
     }
