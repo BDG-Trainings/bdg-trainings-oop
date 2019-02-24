0a3b651c1d6b5eafce6253e15f9a62caf09f87b0
elmira_k82 <elmirakirakosyan@gmail.com>
2019-02-24 16:33:19 +0400

    Bookstorage entry classes

---
+++ src/com/bdg/bookstorage/ekirakosyan/entity/Author.java
@@ -0,0 +1,49 @@
+package com.bdg.bookstorage.ekirakosyan.entity;
+
+import com.bdg.bookstorage.ekirakosyan.common.Gender;
+
+public class Author {
+
+    private String name;
+    private String surname;
+    private Gender gender;
+
+    public Author(String name, String surname, Gender gender) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
