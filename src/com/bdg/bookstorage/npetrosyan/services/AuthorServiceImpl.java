e1dc747452f026bddf60bac3af0016cbe4e2f6b6
narekp92 <npetrosyan1991@gmail.com>
2019-02-24 18:24:26 +0400

    AuthorServiceImpl BookServiceImpl Done

---
+++ src/com/bdg/bookstorage/npetrosyan/services/AuthorServiceImpl.java
@@ -0,0 +1,26 @@
+package com.bdg.bookstorage.npetrosyan.services;
+
+import com.bdg.bookstorage.npetrosyan.common.AuthorCreateParameter;
+import com.bdg.bookstorage.npetrosyan.entity.Author;
+
+public class AuthorServiceImpl implements AuthorService {
+    @Override
+    public Author create(AuthorCreateParameter createParameter) {
+        return null;
+    }
+
+    @Override
+    public Author update() {
+        return null;
+    }
+
+    @Override
+    public Author get() {
+        return null;
+    }
+
+    @Override
+    public boolean delete() {
+        return false;
+    }
+}
