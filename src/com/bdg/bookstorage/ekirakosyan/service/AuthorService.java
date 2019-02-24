4da0da73fe703d2a8f0afad256888e6ebff739fc
elmira_k82 <elmirakirakosyan@gmail.com>
2019-02-24 17:41:01 +0400

    updated AuthorService

--- src/com/bdg/bookstorage/ekirakosyan/service/AuthorService.java
+++ src/com/bdg/bookstorage/ekirakosyan/service/AuthorService.java
@@ -1,11 +1,12 @@
 package com.bdg.bookstorage.ekirakosyan.service;
 
 import com.bdg.bookstorage.ekirakosyan.entity.Author;
+import com.bdg.bookstorage.npetrosyan.createUpdateProject.AuthorCreateParameter;
 
 public interface AuthorService {
 
     Author get();
-    Author create();
+    Author create(AuthorCreateParameter authorCreateParameter );
     Author update();
     boolean delete();
 }
