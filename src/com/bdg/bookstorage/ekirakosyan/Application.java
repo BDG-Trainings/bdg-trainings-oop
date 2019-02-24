97f1fc14c46b47bad9418b5b0d213c67199eb5a3
elmira_k82 <elmirakirakosyan@gmail.com>
2019-02-24 18:00:03 +0400

    added BookUpdateParameter class

--- src/com/bdg/bookstorage/ekirakosyan/Application.java
+++ src/com/bdg/bookstorage/ekirakosyan/Application.java
@@ -8,14 +8,12 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Author author1 = new Author(1, "Joe", "Doe", Gender.MALE);
-        Author author2 = new Author(2, "Jana", "Booth", Gender.FEMALE);
-        Author[] authors = {author1, author2};
+//        Author author1 = new Author(1, "Joe", "Doe", Gender.MALE);
+//        Author author2 = new Author(2, "Jana", "Booth", Gender.FEMALE);
+//        Author[] authors = {author1, author2};
 
 
-        Book book = new Book(1, "Book 1", "Title Here", 5.00, authors);
 
-        System.out.println(book);
 
     }
 
