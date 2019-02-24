87d1f747d8445568fb0745ff9de3845faf3b7f11
Aida Ghalayan <galayanaida@gmail.ru>
2019-02-24 16:35:41 +0400

    entity, enum

---
+++ src/com/bdg/bookstorage/agalayan/entity/Author.java
@@ -0,0 +1,16 @@
+package com.bdg.bookstorage.agalayan.entity;
+
+public class Author {
+    private  String name;
+    private  String surname;
+    private  String gender;
+
+    public Author(String name, String surname, String gender){
+        this.gender=gender;
+        this.name =name;
+        this.surname=surname;
+
+    }
+
+
+}
