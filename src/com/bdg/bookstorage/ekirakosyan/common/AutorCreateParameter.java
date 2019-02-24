e5e38bc1da3ad76104ba4293554bf5dc98efa546
elmira_k82 <elmirakirakosyan@gmail.com>
2019-02-24 17:39:49 +0400

    crated AutorCreateParameter class

---
+++ src/com/bdg/bookstorage/ekirakosyan/common/AutorCreateParameter.java
@@ -0,0 +1,38 @@
+package com.bdg.bookstorage.ekirakosyan.common;
+
+public class AutorCreateParameter {
+
+    private String name;
+    private String surname;
+    private Gender gender;
+
+    public AutorCreateParameter(final String name, final String surname, final Gender gender) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
