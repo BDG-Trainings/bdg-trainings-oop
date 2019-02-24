c44fcd5b0a70f73d7cf30737f410b91ad377fd19
elmira_k82 <elmirakirakosyan@gmail.com>
2019-02-24 18:10:53 +0400

    fixes

--- src/com/bdg/bookstorage/ekirakosyan/service/AuthorService.java
+++ src/com/bdg/bookstorage/ekirakosyan/service/AuthorService.java
@@ -1,12 +1,12 @@
 package com.bdg.bookstorage.ekirakosyan.service;
 
+import com.bdg.bookstorage.ekirakosyan.common.AutorCreateParameter;
 import com.bdg.bookstorage.ekirakosyan.entity.Author;
-import com.bdg.bookstorage.npetrosyan.createUpdateProject.AuthorCreateParameter;
 
 public interface AuthorService {
 
     Author get();
-    Author create(AuthorCreateParameter authorCreateParameter );
+    Author create(AutorCreateParameter autorCreateParameter);
     Author update();
     boolean delete();
 }
