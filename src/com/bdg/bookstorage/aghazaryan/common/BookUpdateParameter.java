95f42b42ca0a1f8ecaa81395cbd1c47b0747928f
Anahit <anahit.ghazaryan91@gmail.com>
2019-02-24 21:03:37 +0400

    BookUpdateParameter updates

--- src/com/bdg/bookstorage/aghazaryan/common/BookUpdateParameter.java
+++ src/com/bdg/bookstorage/aghazaryan/common/BookUpdateParameter.java
@@ -14,13 +14,24 @@ public class BookUpdateParameter {
     public String getName() {
         return name;
     }
+    public void setName(String name){
+        this.name = name;
+    }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id){
+        this.id = id;
+    }
+
     public int getPrice() {
         return price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
     }
 
     @Override
