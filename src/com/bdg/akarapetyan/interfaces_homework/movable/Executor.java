5242008804128e60bc76ed4f8d741d8d89c225f6
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-02-25 17:21:26 +0400

    update MovableCircle and Executor

--- src/com/bdg/akarapetyan/interfaces_homework/movable/Executor.java
+++ src/com/bdg/akarapetyan/interfaces_homework/movable/Executor.java
@@ -5,9 +5,24 @@ public class Executor {
     public static void main(String[] args) {
 
         MovablePoint movablePoint = new MovablePoint(1,1,2,2);
+        System.out.println(movablePoint.toString());
         movablePoint.moveRight();
         movablePoint.moveUp();
         System.out.println(movablePoint.toString());
+        System.out.println();
+
+        MovableCircle movableCircle1 = new MovableCircle(2,2,3,3);
+        System.out.println(movableCircle1.toString());
+        movableCircle1.moveDown();
+        movableCircle1.moveLeft();
+        System.out.println(movableCircle1.toString());
+        System.out.println();
+
+        MovableCircle movableCircle2 = new MovableCircle(11,11,5,5);
+        System.out.println(movableCircle2.toString());
+        movableCircle2.moveDown();
+        movableCircle2.moveLeft();
+        System.out.println(movableCircle2.toString());
 
     }
 }
--- src/com/bdg/akarapetyan/interfaces_homework/movable/MovableCircle.java
+++ src/com/bdg/akarapetyan/interfaces_homework/movable/MovableCircle.java
@@ -1,41 +1,42 @@
 package com.bdg.akarapetyan.interfaces_homework.movable;
 
-public class MovableCircle extends MovablePoint implements  Movable{
+public class MovableCircle extends MovablePoint implements Movable{
 
     private int radius;
     private MovablePoint center;
 
     public MovableCircle(int x, int y, int xSpeed, int ySpeed) {
         super(x, y, xSpeed, ySpeed);
+        center = new MovablePoint(x,y,xSpeed,ySpeed);
     }
 
     @Override
     public String toString() {
         return "MovableCircle{" +
-                "x=" + x +
-                ", y=" + y +
-                ", xSpeed=" + xSpeed +
-                ", ySpeed=" + ySpeed +
+                "x=" + center.x +
+                ", y=" + center.y +
+                ", xSpeed=" + center.xSpeed +
+                ", ySpeed=" + center.ySpeed +
                 '}';
     }
 
     @Override
     public void moveUp() {
-
+        center.moveUp();
     }
 
     @Override
     public void moveDown() {
-
+        center.moveDown();
     }
 
     @Override
     public void moveLeft() {
-
+        center.moveLeft();
     }
 
     @Override
     public void moveRight() {
-
+        center.moveRight();
     }
 }
