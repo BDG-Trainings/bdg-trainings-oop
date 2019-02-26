929713d5c77c84b31a2472a4d02dc472ea13a8b9
Vika <vika.karamyan@gmail.com>
2019-02-26 23:00:45 +0400

    Add Storage class

---
+++ src/com/bdg/bookstorage/vkaramyan/storage/AuthorStorageImpl.java
@@ -0,0 +1,39 @@
+package com.bdg.bookstorage.vkaramyan.storage;
+
+import com.bdg.bookstorage.vkaramyan.entity.Author;
+
+public class AuthorStorageImpl implements AuthorStorage{
+	
+	int currentStorageSize;
+	
+	public AuthorStorageImpl(final int currentStorageSize) {
+		this.currentStorageSize = currentStorageSize;
+	}
+
+	@Override
+	public Author authorStore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Author store() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Author get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
