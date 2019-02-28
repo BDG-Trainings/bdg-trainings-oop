b9575ab048f998d358d358aa00d40d522c84f674
Anahit <anahit.ghazaryan91@gmail.com>
2019-03-01 00:54:24 +0400

    PhoneNumber

--- src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/PhoneNumber.java
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/PhoneNumber.java
@@ -6,6 +6,10 @@ public class PhoneNumber {
     private String number;
     private PhoneType type;
 
-    public PhoneNumber (CountryCode countryCode, String number, PhoneType type )
+    public PhoneNumber (CountryCode countryCode, String number, PhoneType type ){
+        this.countryCode = countryCode;
+        this.number = number;
+        this.type = type;
+    }
 
 }
