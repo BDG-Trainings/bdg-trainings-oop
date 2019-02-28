445d54a63e490ff71ed092f053f3c49d216c4361
Anahit <anahit.ghazaryan91@gmail.com>
2019-03-01 00:53:08 +0400

    PhoneType

---
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/PhoneNumber.java
@@ -0,0 +1,11 @@
+package com.bdg.aghazaryan.src.aghazaryan.src.bankaccountsystem;
+
+public class PhoneNumber {
+
+    private CountryCode countryCode;
+    private String number;
+    private PhoneType type;
+
+    public PhoneNumber (CountryCode countryCode, String number, PhoneType type )
+
+}
