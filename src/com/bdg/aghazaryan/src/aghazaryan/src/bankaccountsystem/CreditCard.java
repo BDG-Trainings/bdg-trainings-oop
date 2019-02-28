849353938be9ad9c91cab573a79fef86892e7d5b
Anahit <anahit.ghazaryan91@gmail.com>
2019-03-01 01:33:01 +0400

    CreditCard

--- src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/CreditCard.java
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/CreditCard.java
@@ -1,11 +1,15 @@
 package com.bdg.aghazaryan.src.aghazaryan.src.bankaccountsystem;
 
+import com.bdg.aghazaryan.src.aghazaryan.src.classexercises.Date;
+
 public class CreditCard {
 
     private String cardNumber;
     private int accountNumber;
     private Date expirationDate;
     private CardType cardType;
+
+
 
     public CreditCard (int accountNumber, Date expirationDate, CardType cardType ){
         this.accountNumber = accountNumber;
