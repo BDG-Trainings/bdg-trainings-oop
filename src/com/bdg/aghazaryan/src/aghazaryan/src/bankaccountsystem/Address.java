a34ca408c9671e8332981f6bf63d2fe74021ece9
Anahit <anahit.ghazaryan91@gmail.com>
2019-03-01 00:15:56 +0400

    Country

--- src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/Address.java
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/Address.java
@@ -1,4 +1,9 @@
 package com.bdg.aghazaryan.src.aghazaryan.src.bankaccountsystem;
 
 public class Address {
+
+
+
+
+
 }
---
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/Country.java
@@ -0,0 +1,33 @@
+package com.bdg.aghazaryan.src.aghazaryan.src.bankaccountsystem;
+
+public enum Country {
+    AM( "Armenia", "051"),
+    USA("New York", "10003"),
+    US("San Francisco", "94110"),
+    IT("Florence", "50123"),
+    FR("Nice", "06200");
+
+    private final String countryName;
+    private final String numericCode;
+
+   Country(final String countryName, final String countryCapital){
+        this.countryName = countryName;
+        this.numericCode = countryCapital;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCountryCapital() {
+        return numericCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "countryName='" + countryName + '\'' +
+                ", numericCode='" + numericCode + '\'' +
+                '}';
+    }
+}
