42ed3ede8636018f1c9d9f579ace83de9664490d
Anahit <anahit.ghazaryan91@gmail.com>
2019-02-28 23:25:39 +0400

    Account

---
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/Account.java
@@ -0,0 +1,10 @@
+package com.bdg.aghazaryan.src.aghazaryan.src.bankaccountsystem;
+
+public class Account {
+
+    private Customer customer;
+    private AccountDetail accountDetail;
+    private AccountBalance accountBalance;
+
+
+}
