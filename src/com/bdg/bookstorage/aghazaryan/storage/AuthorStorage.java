284a2c8f7e87c126230c7f2ce7d032cf0a846adf
Anahit <anahit.ghazaryan91@gmail.com>
2019-02-28 23:04:22 +0400

    AuthorStorage

--- src/com/bdg/bookstorage/aghazaryan/storage/AuthorStorage.java
+++ src/com/bdg/bookstorage/aghazaryan/storage/AuthorStorage.java
@@ -1,4 +1,28 @@
 package com.bdg.bookstorage.aghazaryan.storage;
+import com.bdg.bookstorage.aghazaryan.service.AuthorServiceImpl;
 
-public class AuthorStorage {
+public class AuthorStorage extends AuthorServiceImpl{
+
+    private int currentStorageSize;
+
+
+    public AuthorStorage(int currentStorageSize){
+        this.currentStorageSize = currentStorageSize;
+
+    }
+
+    public int getCurrentStorageSize(){
+        return currentStorageSize;
+    }
+
+    public void setCurrentStorageSize(int currentStorageSize) {
+        this.currentStorageSize = currentStorageSize;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorStorage{" +
+                "currentStorageSize=" + currentStorageSize +
+                '}';
+    }
 }
