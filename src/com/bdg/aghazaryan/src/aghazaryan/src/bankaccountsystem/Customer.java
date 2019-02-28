121f200fad41c7d7084a5ad7d0cf7c3f458d8644
Anahit <anahit.ghazaryan91@gmail.com>
2019-02-28 23:48:04 +0400

    Customer

--- src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/Customer.java
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/Customer.java
@@ -1,4 +1,49 @@
 package com.bdg.aghazaryan.src.aghazaryan.src.bankaccountsystem;
 
 public class Customer {
+
+    private String customerName;
+    private String customerSurname;
+    private Address customerAddress;
+
+    public Customer (String customerName, String customerSurname, Address customerAddress ){
+        this.customerName = customerName;
+        this.customerSurname = customerSurname;
+        this.customerAddress = customerAddress;
+
+    }
+
+      public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public String getCustomerSurname(){
+        return customerSurname;
+    }
+
+    public void setCustomerSurname(String customerSurname) {
+        this.customerSurname = customerSurname;
+    }
+
+    public Address getCustomerAddress(){
+        return customerAddress;
+    }
+
+    public void setcustomerAddress(Address customerAddress){
+        this.customerAddress = customerAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerName='" + customerName + '\'' +
+                ", customerSurname='" + customerSurname + '\'' +
+                ", customerAddress=" + customerAddress +
+                '}';
+    }
 }
+
+
