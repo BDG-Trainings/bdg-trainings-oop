92ac15949e3051775c289e6031aeddc985b65315
Sergey Papoyan <spapoyan87@gmail.com>
2019-03-01 13:02:19 +0400

    enumCountry

--- src/com/bdg/spapoyan/bankAccountSystem/enumBankAccountSystem/Country.java
+++ src/com/bdg/spapoyan/bankAccountSystem/enumBankAccountSystem/Country.java
@@ -2,11 +2,9 @@ package com.bdg.spapoyan.bankAccountSystem.enumBankAccountSystem;
 
 public enum Country {
 
-    AM("Armenia","051");
-
-
-
-
+    AM("Armenia","051"),
+    RU("Russian Federation", "643"),
+    US("United States of America", "840");
 
     private final String countryName;
     private final String numericCode;
