8d06e03474ae1b3a2e491efa604972d8952dd1c3
Sergey Papoyan <spapoyan87@gmail.com>
2019-03-01 14:56:04 +0400

    cardNumber

--- src/com/bdg/spapoyan/bankAccountSystem/Date.java
+++ src/com/bdg/spapoyan/bankAccountSystem/Date.java
@@ -1,36 +1,30 @@
 package com.bdg.spapoyan.bankAccountSystem;
-
 public class Date {
-
         private int month;
         private int year;
 
-        public Date (int month, int year) {
-           this.month = month;
-           this.year = year;
-        }
+    public Date(int month, int year) {
+        this.month = month; this.year = year;
+    }
 
-        public int getMonth() {
+    public int getMonth() { return month; }
 
-            return month;
-        }
+    public int getYear() { return year; }
 
-        public int getYear() {
-
-            return year;
-        }
-
-        public void setMonth(int month) {
+    public void setMonth(int month) {
             this.month = month;
             if (month >= 1 && month <= 12) {
                 this.month = month;
             }
         }
-
-        public void setYear(int year) {
+    public void setYear(int year) {
             this.year = year;
             if (year >= 1900 && year <= 9999) {
                 this.year = year;
             }
         }
+
+    public String toString() {
+        String total = "mm/yyyy"; return "mm/yyyy";
+    }
 }
---
+++ src/com/bdg/spapoyan/bankAccountSystem/Test.java
@@ -0,0 +1,11 @@
+package com.bdg.spapoyan.bankAccountSystem;
+
+import com.bdg.spapoyan.bankAccountSystem.enumBankAccountSystem.CardType;
+
+public class Test {
+
+    public static void main(String[] args) {
+     //  CreditCard creditCard = new CreditCard(1,3, ,CardType.VISA);
+     //  System.out.println(creditCard.cardNumber());
+    }
+}
