8274b0e1e98bed88796a898ff696c5b7c74c4e88
narekp92 <npetrosyan1991@gmail.com>
2019-03-02 00:04:58 +0400

    SnakeGame pushed

---
+++ src/com/bdg/npetrosyan/snake_game/MainWindow.java
@@ -0,0 +1,24 @@
+package com.bdg.npetrosyan.snake_game;
+
+import javax.swing.*;
+
+/**
+ * Created by Narek Petrosyan 23.02.2019
+ */
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        setTitle("Snake");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+
+
+}
