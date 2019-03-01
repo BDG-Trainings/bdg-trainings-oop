34d7fd3dbc8c7983083586b3127887c3e361e5d8
Sergey Papoyan <spapoyan87@gmail.com>
2019-03-01 15:39:37 +0400

    Account

--- src/com/bdg/spapoyan/bankAccountSystem/Test.java
+++ src/com/bdg/spapoyan/bankAccountSystem/Test.java
@@ -1,11 +1,8 @@
 package com.bdg.spapoyan.bankAccountSystem;
 
-import com.bdg.spapoyan.bankAccountSystem.enumBankAccountSystem.CardType;
 
 public class Test {
 
-    public static void main(String[] args) {
-     //  CreditCard creditCard = new CreditCard(1,3, ,CardType.VISA);
-     //  System.out.println(creditCard.cardNumber());
+    public static void main(String[] args){
     }
 }
