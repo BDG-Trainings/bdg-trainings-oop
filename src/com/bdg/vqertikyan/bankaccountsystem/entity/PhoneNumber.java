8592ffefd1822b8813d2a23bf62ead952be39783
VMA <varduhi.qertikyan@gmail.com>
2019-03-02 21:10:43 +0400

    new PhoneNumber class

--- src/com/bdg/vqertikyan/bankaccountsystem/entity/PhoneNumber.java
+++ src/com/bdg/vqertikyan/bankaccountsystem/entity/PhoneNumber.java
@@ -1,4 +1,49 @@
 package com.bdg.vqertikyan.bankaccountsystem.entity;
 
+import com.bdg.vqertikyan.bankaccountsystem.common.CountryCode;
+import com.bdg.vqertikyan.bankaccountsystem.common.PhoneType;
+
 public class PhoneNumber {
+    private CountryCode countryCode;
+    private String number;
+    private PhoneType type;
+
+    public PhoneNumber(CountryCode countryCode, String number, PhoneType type) {
+        this.countryCode = countryCode;
+        this.number = number;
+        this.type = type;
+    }
+
+    public CountryCode getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(CountryCode countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public PhoneType getType() {
+        return type;
+    }
+
+    public void setType(PhoneType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneNumber{" +
+                "countryCode=" + countryCode +
+                ", number='" + number + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
