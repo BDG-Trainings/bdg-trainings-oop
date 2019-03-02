26cba2279f0cd79c28a084291c126c475769aa41
Vika <vika.karamyan@gmail.com>
2019-03-02 12:47:46 +0400

    Update CountryCode class

--- src/com/bdg/vkaramyan/bankaccountsystem/CountryCode.java
+++ src/com/bdg/vkaramyan/bankaccountsystem/CountryCode.java
@@ -2,18 +2,18 @@ package com.bdg.vkaramyan.bankaccountsystem;
 
 public enum CountryCode {
 	
-	ARM ("+374", Country),
-	RUS ("+7", "Russia"),
-	IT  ("+39", "Italy");
+	ARM ("+374", Country.AM),
+	RUS ("+7", Country.RUS),
+	IT  ("+39", Country.IT);
 	
 	
 	private final String countryCode;
 	private final Country country;
 	
-	private CountryCode(final String countryCode, final Country country) {
-		
+	private CountryCode(String countryCode, Country country) {
 		this.countryCode = countryCode;
 		this.country = country;
 	}
 	
 }
+
