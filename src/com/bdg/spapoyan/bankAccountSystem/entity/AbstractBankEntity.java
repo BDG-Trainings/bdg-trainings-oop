4b23b1004d0109738561fa8bf8d54d900e01fbdf
Sergey Papoyan <spapoyan87@gmail.com>
2019-03-02 17:38:02 +0400

    AbstractBankEntity.LocalDate

--- src/com/bdg/spapoyan/bankAccountSystem/entity/AbstractBankEntity.java
+++ src/com/bdg/spapoyan/bankAccountSystem/entity/AbstractBankEntity.java
@@ -1,12 +1,13 @@
 package com.bdg.spapoyan.bankAccountSystem.entity;
 
-import com.bdg.spapoyan.Date;
+import java.time.LocalDate;
 
 public abstract class AbstractBankEntity {
 
     protected int id;
-    protected Date createdDate;
-    protected Date updatedDate;
+    protected LocalDate createdDate;
+    protected LocalDate updatedDate;
+    protected LocalDate deletedDate;
 
     public AbstractBankEntity(int id) {
         this.id = id;
