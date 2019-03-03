7bb9914d6ec5e941eaec3cd5cf57c73111a9849b
William Arustamyan <williamarustamyan@workfront.com>
2019-03-03 18:46:17 +0400

    Simple json parser. (PhoneNumber class)

---
+++ src/com/bdg/master/bankaccountsystem/PhoneTypeNotFoundException.java
@@ -0,0 +1,16 @@
+package com.bdg.master.bankaccountsystem;
+
+
+public final class PhoneTypeNotFoundException extends RuntimeException {
+
+    private static final String DEFAULT_NOT_FOUND_MESSAGE = "Phone Type %s not found";
+    private final String message;
+
+    public PhoneTypeNotFoundException(final String message) {
+        this.message = String.format(DEFAULT_NOT_FOUND_MESSAGE, message);
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
