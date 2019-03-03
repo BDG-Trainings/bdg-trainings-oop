ea96615ff1d47cb936fb248a7b9915b3e71812a0
OfeliaSSK <sahakyanofelia@gmail.com>
2019-03-03 18:54:20 +0400

    Merge branch 'osahakyan' of C:\Users\User\IdeaProjects\bdg-trainings-oop.HW with conflicts.

--- src/com/bdg/osahakyan/bankaccountsystem/enums/CountryCode.java
+++ src/com/bdg/osahakyan/bankaccountsystem/enums/CountryCode.java
@@ -1,26 +1,18 @@
 package com.bdg.osahakyan.bankaccountsystem.enums;
 
-public enum CountryCode {
+public enum  CountryCode {
 
 //source--> https://countrycode.org/
 
-    ARM("+374", Country.AM),
-    USA("+1", Country.USA),
-    CHN("+86", Country.CHN);
+    ARM("+374", "Armenia"),
+    USA("+1", "United States of America" ),
+    CHN("+86", "China");
 
-    private final String countryCode;
-    private final Country country;
+    private String countryCode;
+    private Country country;
 
-    CountryCode(final String countryCode, final Country country) {
+    CountryCode(String countryCode, Country country) {
         this.countryCode = countryCode;
         this.country = country;
-    }
-
-    public String getCountryCode() {
-        return this.countryCode;
-    }
-
-    public Country getCountry() {
-        return this.country;
     }
 }
