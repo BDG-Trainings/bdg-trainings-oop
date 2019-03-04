92eee8f7e0ddf5df2f8cff120bf05c8adbd7e17e
Anahit <anahit.ghazaryan91@gmail.com>
2019-03-05 00:19:14 +0400

    AccountBalance update

--- src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/AccountBalance.java
+++ src/com/bdg/aghazaryan/src/aghazaryan/src/bankaccountsystem/AccountBalance.java
@@ -1,4 +1,18 @@
 package com.bdg.aghazaryan.src.aghazaryan.src.bankaccountsystem;
 
 public class AccountBalance {
+
+    private double balance;
+
+    public AccountBalance (double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
 }
