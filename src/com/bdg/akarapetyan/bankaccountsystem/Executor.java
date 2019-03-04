5d0e888cc447c9305f2ddc6e83ac075b53e3b4e7
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-03-04 09:43:34 +0400

    update bankaccountsystem

---
+++ src/com/bdg/akarapetyan/bankaccountsystem/Executor.java
@@ -0,0 +1,11 @@
+package com.bdg.akarapetyan.bankaccountsystem;
+
+public class Executor {
+
+    public static void main(String[] args) {
+
+
+        Address address1 = new Address(Country.IT, "Moscovyan", "094566747" );
+        Address address2 = new Address(Country.IT, "Pushkin", "+37455222222");
+    }
+}
