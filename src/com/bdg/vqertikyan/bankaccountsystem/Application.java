07e50744e2763e7f7c1667ea1d69fb0b0f6b6e81
varduhi <varduhi.qertikyan@gmail.com>
2019-03-04 11:53:03 +0400

    random

--- src/com/bdg/vqertikyan/bankaccountsystem/Application.java
+++ src/com/bdg/vqertikyan/bankaccountsystem/Application.java
@@ -1,7 +1,12 @@
 package com.bdg.vqertikyan.bankaccountsystem;
 
+import com.bdg.vqertikyan.bankaccountsystem.common.CommonUtils;
+
 public class Application {
     public static void  main(String[] args){
 
+        CommonUtils utils = new CommonUtils();
+        String creditCardNumber = utils.randomString(16);
+        System.out.println(creditCardNumber);
     }
 }
