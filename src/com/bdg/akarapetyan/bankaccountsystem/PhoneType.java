29bac1e51d0363407cafcfdca583e2c3e4b5add2
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-03-04 12:09:11 +0400

    enum PhoneType

---
+++ src/com/bdg/akarapetyan/bankaccountsystem/PhoneType.java
@@ -0,0 +1,26 @@
+package com.bdg.akarapetyan.bankaccountsystem;
+
+public enum PhoneType {
+
+    Mobile("mobile"), Home("Home"), Work("Work");
+
+
+    private final String phoneTypeName;
+
+    PhoneType(final String phoneTypeName) {
+        this.phoneTypeName = phoneTypeName;
+    }
+
+    public static PhoneType findByPhoneTypeName(final String phoneTypeName) {
+        for (final PhoneType pt : values()) {
+            if (pt.phoneTypeName.equalsIgnoreCase(phoneTypeName)) {
+                return pt;
+            }
+        }
+        throw new PhoneTypeNotFoundException(phoneTypeName);
+    }
+
+    public String getPhoneTypeName() {
+        return this.phoneTypeName;
+    }
+}
