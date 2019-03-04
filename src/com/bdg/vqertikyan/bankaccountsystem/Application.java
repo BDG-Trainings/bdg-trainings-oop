62df8fc6889272d451153883ef1fd47c50bd59c8
varduhi <varduhi.qertikyan@gmail.com>
2019-03-04 13:09:05 +0400

    credit card number

--- src/com/bdg/vqertikyan/bankaccountsystem/Application.java
+++ src/com/bdg/vqertikyan/bankaccountsystem/Application.java
@@ -2,11 +2,15 @@ package com.bdg.vqertikyan.bankaccountsystem;
 
 import com.bdg.vqertikyan.bankaccountsystem.common.CommonUtils;
 
+import java.util.Arrays;
+
 public class Application {
     public static void  main(String[] args){
 
         CommonUtils utils = new CommonUtils();
-        String creditCardNumber = utils.randomString(16);
+
+        String noSpaces = utils.randomString(16);
+        String creditCardNumber = noSpaces.substring(0,4)+" "+noSpaces.substring(4,8)+" "+noSpaces.substring(8,12)+" "+noSpaces.substring(12,16);
         System.out.println(creditCardNumber);
     }
 }
