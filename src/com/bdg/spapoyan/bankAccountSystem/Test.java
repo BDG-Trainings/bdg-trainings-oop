dcec5c2a7ef85e7579242130f53a16f0c3abb0b3
Sergey Papoyan <spapoyan87@gmail.com>
2019-03-10 21:20:40 +0400

    .

--- src/com/bdg/spapoyan/bankAccountSystem/Test.java
+++ src/com/bdg/spapoyan/bankAccountSystem/Test.java
@@ -1,14 +1,8 @@
 package com.bdg.spapoyan.bankAccountSystem;
 
-
-import com.bdg.spapoyan.bankAccountSystem.enumBankAccountSystem.CardType;
-
 public class Test {
 
     public static void main(String[] args){
 
-    //CreditCard creditCard = new CreditCard(1, ,3,new Date(3,2019),CardType.MASTERCARD);
-
-       // System.out.println(creditCard.toString());
     }
 }
