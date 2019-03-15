bfe9cf556e4d518451e83163ab0129d52113e997
OfeliaSSK <sahakyanofelia@gmail.com>
2019-03-15 17:14:29 +0400

    ArrayList, LinkedList

---
+++ src/com/bdg/osahakyan/collection/LinkedList.java
@@ -0,0 +1,72 @@
+package com.bdg.osahakyan.collection;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class LinkedList<Е> implements Collection<Е> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+}
