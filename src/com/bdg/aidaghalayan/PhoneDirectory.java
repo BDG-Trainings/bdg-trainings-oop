af2945fd19b7e91176ae989a8ded356905442683
Aida Ghalayan <galayanaida@gmail.ru>
2019-03-23 22:22:16 +0400

    PhoneDirectory

---
+++ src/com/bdg/aidaghalayan/PhoneDirectory.java
@@ -0,0 +1,43 @@
+package com.bdg.aidaghalayan;
+
+import java.util.HashMap;
+
+public class PhoneDirectory {
+    private HashMap<String, String> hashMap = new HashMap<>();
+
+
+    public PhoneDirectory(String name, String number) {
+        hashMap.put(name, number);
+
+
+    }
+
+    private boolean find(String name) {
+        return hashMap.get(name) != null;
+    }
+
+    private String getNumber(String name) {
+        return hashMap.get(name);
+    }
+    private void putNumber(String name, String number){
+        hashMap.put(name, number);
+    }
+    private void removeContact(String name){
+        hashMap.remove(name);
+    }
+
+
+    public static void main(String[] args) {
+        PhoneDirectory directory = new PhoneDirectory("Anna", "098524163");
+        directory.putNumber("Davit", "095623145");
+
+        System.out.println(directory.find("sdf"));
+        System.out.println(directory.getNumber("Davit"));
+        directory.removeContact("Anna");
+        System.out.println(directory.getNumber("Anna"));
+
+
+
+    }
+
+}
