6151e6d9eab3a0dee6c8b49dd4f620614e4a11a7
narekp92 <npetrosyan1991@gmail.com>
2019-03-25 19:58:55 +0400

    Massive2

--- src/com/bdg/npetrosyan/homeworks/App220.java
+++ src/com/bdg/npetrosyan/homeworks/App220.java
@@ -1,4 +1,24 @@
 package com.bdg.npetrosyan.homeworks;
-
+//bacasakan ev drakan unecoxneri qanak@
 public class App220 {
+    public static void main(String[] args) {
+        int[] y = new int[6];
+        y[0] = 95;
+        y[1] = -5;
+        y[2] = -15;
+        y[3] = 4;
+        y[4] = 3;
+        y[5] = 10;
+
+        int negativeIndex = 0;
+        int positiveIndex = 0;
+        for(int i = 0; i < y.length; i++){
+            if (y[i] > 0){
+                positiveIndex++;
+            }else {
+                negativeIndex++;
+            }
+        }
+        System.out.println("Positive Index "+positiveIndex + ", Negative Index "+negativeIndex);
+    }
 }
