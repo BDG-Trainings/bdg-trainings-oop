e1855273363c0e5fc2e568092d2c9fb22feec5a8
OfeliaSSK <sahakyanofelia@gmail.com>
2019-03-26 17:51:42 +0400

    PhoneDictionary

---
+++ src/com/bdg/osahakyan/hashmap/PhoneDictionaryImpl.java
@@ -0,0 +1,14 @@
+package com.bdg.osahakyan.hashmap;
+
+
+
+public class PhoneDictionaryImpl  {
+
+
+    public PhoneDictionary find(String name){return null;}
+    public PhoneDictionary getNumber(String name){return null;}
+    public PhoneDictionary putNumber(String name, String number){return null;}
+    public PhoneDictionary removeContact(String name){return null;}
+
+}
+
