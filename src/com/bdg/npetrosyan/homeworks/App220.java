6913b010dce1d037d8ce0a0fcb438cfc9e6c6607
narekp92 <npetrosyan1991@gmail.com>
2019-03-27 17:08:39 +0400

    Massive3

--- src/com/bdg/npetrosyan/homeworks/App220.java
+++ src/com/bdg/npetrosyan/homeworks/App220.java
@@ -1,6 +1,7 @@
 package com.bdg.npetrosyan.homeworks;
 //bacasakan ev drakan unecoxneri qanak@
 public class App220 {
+//positive and negative numbers count
     public static void main(String[] args) {
         int[] y = new int[6];
         y[0] = 95;
@@ -10,15 +11,15 @@ public class App220 {
         y[4] = 3;
         y[5] = 10;
 
-        int negativeIndex = 0;
-        int positiveIndex = 0;
+        int negativeNumbers = 0;
+        int positiveNumbers = 0;
         for(int i = 0; i < y.length; i++){
             if (y[i] > 0){
-                positiveIndex++;
+                positiveNumbers++;
             }else {
-                negativeIndex++;
+                negativeNumbers++;
             }
         }
-        System.out.println("Positive Index "+positiveIndex + ", Negative Index "+negativeIndex);
+        System.out.println("Positive Numbers Count "+positiveNumbers + ", Negative Numbers Count "+negativeNumbers);
     }
 }
