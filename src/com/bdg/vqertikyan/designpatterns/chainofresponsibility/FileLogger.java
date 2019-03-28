e2f7de59b1ae8e7d8294908c27591221ced0a167
VMA <varduhi.qertikyan@gmail.com>
2019-03-28 21:35:11 +0400

    chain of responsibility

---
+++ src/com/bdg/vqertikyan/designpatterns/chainofresponsibility/FileLogger.java
@@ -0,0 +1,14 @@
+package com.bdg.vqertikyan.designpatterns.chainofresponsibility;
+
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
