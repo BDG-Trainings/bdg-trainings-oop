755bc789a955dd1e3b92cf6d029ee4fbbd57bd1e
VMA <varduhi.qertikyan@gmail.com>
2019-04-02 20:35:29 +0400

    mvc pattern

---
+++ src/com/bdg/vqertikyan/designpatterns/mvc/StudentView.java
@@ -0,0 +1,10 @@
+package com.bdg.vqertikyan.designpatterns.mvc;
+
+public class StudentView {
+
+    public void printStudentDetails(String studentName, String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
