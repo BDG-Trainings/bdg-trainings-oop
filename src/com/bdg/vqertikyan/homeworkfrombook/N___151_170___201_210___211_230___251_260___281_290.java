9fc6638f9d810d300ce6f4cfeb6b61d91b9dcfbe
VMA <varduhi.qertikyan@gmail.com>
2019-04-19 23:39:59 +0400

    151-290

--- src/com/bdg/vqertikyan/homeworkfrombook/N___151_170___201_210___211_230___251_260___281_290.java
+++ src/com/bdg/vqertikyan/homeworkfrombook/N___151_170___201_210___211_230___251_260___281_290.java
@@ -10,10 +10,53 @@ public class N___151_170___201_210___211_230___251_260___281_290 {
         //Scanner count = new Scanner(System.in);
         //int n = Integer.parseInt(count.nextLine());
 
-        int n = 5;
+        int n = 10;
+        int n1 = 32;
         int[] x;
-        x = new int[]{5, 4, 3, 4, 5};
+        x = new int[]{5, 8, 4, 3, 4, 5, 7, 8, 0, 5};
         int count = 0;
+
+
+        //164 ?????
+        for (int h = 10; h < 31; h++) {
+            if (h > n1) {
+                System.out.println("164: " + h*h);
+                return;
+            }
+        }
+
+        //201
+        int number = 5454654;
+
+        while (number / 10 != 0) {
+            number = number / 10;
+            count++;
+        }
+        count++;
+        System.out.println("201: " + count);
+
+
+        //215
+        int sum = 0;
+        for(int b = 0; b < n; b++){
+            if (b%2 == 0){
+                sum = sum + x[b];
+            }
+        }
+        System.out.println("215: " + sum);
+
+
+
+        //257
+        int max = 0;
+        int index = 0;
+        for (int j = 0; j < n; j++) {
+            if (x[j] > max) {
+                max = x[j];
+                index = j;
+            }
+        }
+        System.out.println("257: " + index);
 
 
         //281
@@ -33,6 +76,6 @@ public class N___151_170___201_210___211_230___251_260___281_290 {
             i++;
         }
 
-        System.out.println(Arrays.toString(y));
+        System.out.println("281: " + Arrays.toString(y));
     }
 }
