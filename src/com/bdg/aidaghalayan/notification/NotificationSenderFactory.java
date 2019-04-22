3065c76ea52e5334847118e82232b90dfc11c534
William Arustamyan <williamarustamyan@workfront.com>
2019-04-22 19:55:52 +0400

    Code review refactoring

--- src/com/bdg/aidaghalayan/notification/NotificationSenderFactory.java
+++ src/com/bdg/aidaghalayan/notification/NotificationSenderFactory.java
@@ -3,12 +3,19 @@ package com.bdg.aidaghalayan.notification;
 import java.util.HashMap;
 import java.util.Map;
 
-public class NotificationSenderFactory<NotificationSender> {
+public class NotificationSenderFactory {
 
 
-   private Map <NotificationType, ? extends NotificationSender> senderMap= new HashMap <>();
+    private final Map<NotificationType, NotificationSender> registry = new HashMap<>(NotificationType.values().length);
 
+    public NotificationSenderFactory() {
+        this.registry.put(NotificationType.CALL, CallNotificationSender.getInstance());
+        this.registry.put(NotificationType.EMAIL, EmailNotificationSender.getInstance());
+        this.registry.put(NotificationType.SMS, MobileSMSNotificationSender.getInstance());
+    }
 
-
+    public NotificationSender getNotification(final NotificationType type) {
+        return this.registry.get(type);
+    }
 
 }
